2d8ad206c050c2f650f7c923d4ee763f7c376b12
kevinchen147 <xxgj@outlook.com>
2022-01-03 14:14:10 +0800

    feat(config.fish): update shell to fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,32 @@
+if status is-interactive
+    # +----+
+    # | Vi |
+    # +----+
+    fish_vi_key_bindings
+
+    # +------+
+    # | Path |
+    # +------+
+    if test -d "$HOME/.local/bin/"
+        fish_add_path -m "$HOME/.local/bin/"
+    end
+    if test -d "/usr/local/go/bin"
+        fish_add_path -m "/usr/local/go/bin"
+    end
+
+    # +------+
+    # | Abbr |
+    # +------+
+    abbr --add g "git"
+    abbr --add .... "cd ../../.."
+    abbr --add ... "cd ../.."
+    abbr --add .. "cd .."
+    abbr --add - "cd -"
+    abbr --add _ "sudo"
+    abbr --add c "code"
+    abbr --add d "sudo docker"
+    abbr --add g "git"
+    abbr --add py "python3"
+    abbr --add on "gsettings set org.gnome.system.proxy mode manual"
+    abbr --add off "gsettings set org.gnome.system.proxy mode none"
+end
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,4 +8,5 @@ rsync --exclude ".git/" \
     --exclude "bootstrap.sh" \
     --exclude "README.md" \
     -avh --no-perms . ~;
+
 source ~/.profile;
