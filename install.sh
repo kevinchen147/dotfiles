6dad3b4506dab57a58029b06fc0e018f091a8d12
kevinchen147 <xxgj@outlook.com>
2022-01-31 18:15:21 +0800

    feat(install.sh): add stow

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # | Development |
 # +-------------+
 sudo apt update \
-&& sudo apt -y install git git-lfs build-essential make \
+&& sudo apt -y install git git-lfs build-essential make stow \
 && sudo apt autoremove -y && sudo apt clean -y \
 && git lfs install \
 && python3 -m pip install --no-cache-dir requests sortedcontainers --user
