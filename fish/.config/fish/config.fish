972d068c1b9e91e5753f100ad239f45efa695973
xxgj <xxgj@outlook.com>
2022-02-08 21:24:20 +0800

    feat(config.fish): add key binding

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,10 +9,12 @@ if status is-interactive
     # +-----+
     set -gx GPG_TTY (tty)
 
-    # +----+
-    # | Vi |
-    # +----+
+    # +-------------+
+    # | Key Binding |
+    # +-------------+
     fish_vi_key_bindings
+    bind -M insert \ci forward-word
+    bind -M insert \co accept-autosuggestion
 
     # +------+
     # | Path |
